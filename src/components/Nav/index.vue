a4b6f40e486312a54a92f319479e7885b3dc9907
wang <1312975595@qq.com>
2019-08-20 10:53:42 +0800

    首页

--- src/components/Nav/index.vue
+++ src/components/Nav/index.vue
@@ -1,17 +1,40 @@
 <template>
   <div class="home-nav">
-    <!-- <div class="nav-one"> -->
-    <div class="swiper-container">
-      <div class="swiper-wrapper">
-        <div class="swiper-slide" v-for="(item) in imgList1" :key="item.imgSrc">
-          <a href="#">
-            <img :src="'http://movie.miguvideo.com/publish/i_www'+ item.imgSrc" alt />
-          </a>
+    <div class="nav-one">
+      <div class="swiper-container swiper1">
+        <div class="swiper-wrapper mycolor">
+          <div class="swiper-slide" v-for="(item) in imgList1" :key="item.imgSrc">
+            <a href="#">
+              <img :src="'http://movie.miguvideo.com/publish/i_www'+ item.imgSrc" alt />
+            </a>
+          </div>
         </div>
       </div>
-      <!-- </div> -->
-      <div class="nav-two">
-        <p>正在售票</p>
+    </div>
+    <div class="nav-two">
+      <p class="mytitle">正在售票</p>
+      <div class="swiper-container swiper2">
+        <div class="swiper-wrapper">
+          <div class="swiper-slide" v-for="item in imgList2" :key="item.imgSrc">
+            <figure>
+              <img :src="'http://movie.miguvideo.com/publish/i_www'+ item.imgSrc" alt />
+              <figcaption>
+                <p>{{item.name}}</p>
+                <p>{{item.LONG_NAME}}</p>
+              </figcaption>
+            </figure>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="nav-three">
+      <p class="mytitle">精彩活动</p>
+      <div class="swiper-container swiper3">
+        <div class="swiper-wrapper">
+          <div class="swiper-slide" v-for="item in imgList3" :key="item.imgSrc">
+            <img :src="'http://movie.miguvideo.com/publish/i_www'+ item.imgSrc" alt />
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -31,10 +54,17 @@ export default {
   },
   methods: {
     initSwiper() {
-      new Swiper('.swiper-container', {
+      var Myswiper1 = new Swiper('.swiper1', {
+        loop: true,
         effect: 'coverflow',
-        slidesPerView: 3,
+        slidesPerView: 2,
         centeredSlides: true
+      })
+      var Myswiper2 = new Swiper('.swiper2', {
+        slidesPerView: 3
+      })
+      var Myswiper3 = new Swiper('.swiper3', {
+        slidesPerView: 1
       })
     },
     ...mapActions('navImg', ['getNavImg'])
@@ -47,30 +77,68 @@ export default {
   }
 }
 </script>
-<style lang="scss" scoped>
-.swiper-container {
-  // height: 340px;
-  width: 100%;
-  background: #fff;
-  border-top: 1px solid #f1f1f1;
-  // border-bottom: 1px solid #f1f1f1;
-  position: relative;
-  .swiper-wrapper {
-    padding-top: 30px;
-    padding-bottom: 30px;
-    // margin: 0 auto;
-    position: relative;
-    // overflow: hidden;
-    // z-index: 1;
-    a {
-      display: inline-block;
-      width: 100%;
-      // height: 100%;
-      z-index: 1;
-      img {
-        width: 187.5px;
-        z-index: -9;
+<style lang="scss" >
+.home-nav {
+  .nav-one {
+    background: #efefef;
+  }
+  .swiper1 {
+    .swiper-wrapper {
+      padding-top: 30px;
+      padding-bottom: 30px;
+      a {
+        img {
+          width: 187.5px;
+          z-index: -9;
+        }
       }
+    }
+  }
+  .swiper2 {
+    img {
+      height: 175px;
+    }
+    figcaption p:first-child {
+      line-height: 1.7em;
+      padding-left: 12px;
+      padding-right: 12px;
+      text-overflow: ellipsis;
+      white-space: nowrap;
+      overflow: hidden;
+      font-size: 14px !important;
+      color: #666;
+      text-align: center;
+    }
+    figcaption p:last-child {
+      line-height: 1.7em;
+      padding-left: 12px;
+      padding-right: 12px;
+      text-overflow: ellipsis;
+      white-space: nowrap;
+      overflow: hidden;
+      color: #999;
+      font-size: 13px !important;
+    }
+  }
+  .swiper3 {
+    img {
+      width: 100%;
+      height: 140px;
+    }
+  }
+  .mytitle {
+    position: relative;
+    margin-left: 10px;
+    margin-bottom: 30px;
+    margin-top: 30px;
+    &::before {
+      position: absolute;
+      top: 0;
+      left: -10px;
+      width: 2px;
+      content: '';
+      height: 20px;
+      background: #f74444;
     }
   }
 }
