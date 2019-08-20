cafef7929531440edb0387faaf1ddd4ff9d5ebdf
ChrisChoubaba <709346613@qq.com>
2019-08-20 09:15:18 +0800

    页面title切换完善

--- src/router.js
+++ src/router.js
@@ -10,15 +10,24 @@ const routes = [
     children: [
       {
         path: 'watch',
-        component: () => import('./views/Home/watch.vue')
+        component: () => import('./views/Home/watch.vue'),
+        meta: {
+          title: '咪咕影院-首页'
+        }
       },
       {
         path: 'bookTicket',
-        component: () => import('./views/Home/bookTicket.vue')
+        component: () => import('./views/Home/bookTicket.vue'),
+        meta: {
+          title: '咪咕影院-影片'
+        }
       },
       {
         path: 'personCenter',
-        component: () => import('./views/Home/personCenter.vue')
+        component: () => import('./views/Home/personCenter.vue'),
+        meta: {
+          title: '我的'
+        }
       },
       {
         path: '',
@@ -28,23 +37,39 @@ const routes = [
   },
   {
     path: '/city',
-    component: () => import('./views/City/index.vue')
+    component: () => import('./views/City/index.vue'),
+    meta: {
+      title: '选择城市'
+    }
   },
   {
     path: '/login',
-    component: () => import('./views/Login/index.vue')
+    component: () => import('./views/Login/index.vue'),
+    meta: {
+      title: '咪咕用户中心门户-登录'
+    }
   },
   {
     path: '/register',
-    component: () => import('./views/Register/index.vue')
+    component: () => import('./views/Register/index.vue'),
+    meta: {
+      title: '咪咕用户中心门户-注册'
+    }
   },
   {
     path: '/wallet',
-    component: () => import('./views/Wallet/index.vue')
+    component: () => import('./views/Wallet/index.vue'),
+    meta: {
+      title: '我的电影卡'
+    }
   }
 ]
 
 const router = new VueRouter({
   routes
+})
+router.afterEach((to,from) => {
+  let title =  to.meta.title
+  document.title = title
 })
 export default router
